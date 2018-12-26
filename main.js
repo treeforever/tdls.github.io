1b599a76e5f561243cb6893e8d5732ed67877748
Xiyang Chen <settinghead@gmail.com>
2018-12-26 00:13:55 -0500

    sheet fetch urls

---
+++ main.js
@@ -0,0 +1,8 @@
+assembleEvent(document.getElementById('event-list'));
+
+function assembleEvent() {
+
+}
+
+// https://spreadsheets.google.com/feeds/list/1WghUEANwzE1f8fD_sdTvM9BEmr1C9bZjPlFSIJX9iLE/1/public/values?alt=json
+// https://sheets.googleapis.com/v4/spreadsheets/1WghUEANwzE1f8fD_sdTvM9BEmr1C9bZjPlFSIJX9iLE/values/Schedule?key=AIzaSyAUMihCUtNS35espxycitPYrTE_78W93Ps
