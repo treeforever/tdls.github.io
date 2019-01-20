b37453860b95d9eee6dbbb539b18263248d39f0b
Xiyang Chen <settinghead@gmail.com>
2019-01-20 14:58:41 -0500

    fix anchor scroll across the board

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('past-events'),
     document.getElementById('site-contributors'),
     document.getElementById('useful-links'),
-
   );
 
   registerRoutes();
   handleHashChange(window.location.href);
+  fixOffset();
+
 })
 
 function registerRoutes() {
@@ -597,6 +598,85 @@ function rawRowToRow(rawHeader, rawRow) {
   }
 }
 
+function fixOffset() {
+  const HISTORY_SUPPORT = !!(history && history.pushState);
+
+  const anchorScrolls = {
+    ANCHOR_REGEX: /^#[^ ]+$/,
+    OFFSET_HEIGHT_PX: 120,
+
+    /**
+     * Establish events, and fix initial scroll position if a hash is provided.
+     */
+    init: function () {
+      this.scrollToCurrent();
+      window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
+      document.body.addEventListener('click', this.delegateAnchors.bind(this));
+    },
+
+    /**
+     * Return the offset amount to deduct from the normal scroll position.
+     * Modify as appropriate to allow for dynamic calculations
+     */
+    getFixedOffset: function () {
+      return this.OFFSET_HEIGHT_PX;
+    },
+
+    /**
+     * If the provided href is an anchor which resolves to an element on the
+     * page, scroll to it.
+     * @param  {String} href
+     * @return {Boolean} - Was the href an anchor.
+     */
+    scrollIfAnchor: function (href, pushToHistory) {
+      let match, rect, anchorOffset;
+
+      if (!this.ANCHOR_REGEX.test(href)) {
+        return false;
+      }
+
+      match = document.getElementById(href.slice(1));
+
+      if (match) {
+        rect = match.getBoundingClientRect();
+        anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+        window.scrollTo(window.pageXOffset, anchorOffset);
+
+        // Add the state to history as-per normal anchor links
+        if (HISTORY_SUPPORT && pushToHistory) {
+          history.pushState({}, document.title, location.pathname + href);
+        }
+      }
+
+      return !!match;
+    },
+
+    /**
+     * Attempt to scroll to the current location's hash.
+     */
+    scrollToCurrent: function () {
+      this.scrollIfAnchor(window.location.hash);
+    },
+
+    /**
+     * If the click event's target was an anchor, fix the scroll position.
+     */
+    delegateAnchors: function (e) {
+      const elem = e.target;
+
+      if (
+        elem.nodeName === 'A' &&
+        this.scrollIfAnchor(elem.getAttribute('href'), true)
+      ) {
+        e.preventDefault();
+      }
+    }
+  };
+
+  window.addEventListener(
+    'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+  );
+}
 
 // https://spreadsheets.google.com/feeds/list/1WghUEANwzE1f8fD_sdTvM9BEmr1C9bZjPlFSIJX9iLE/1/public/values?alt=json
 // TODO: implement scrolling
