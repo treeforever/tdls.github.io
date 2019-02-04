fcc531c0306fa1a7abf7454c617992251f871b2e
Mengqi Zhu <treeforever7@gmail.com>
2019-02-03 22:28:21 -0500

    move icons to header

--- components/footer.js
+++ components/footer.js
@@ -4,53 +4,13 @@ export default () => (
       <div style={{ textAlign: 'center' }}>
         <div className="pull-left copyright">
           &copy; 2018-2019 Aggregate Intellect Inc.
-      </div>
-        <ul className="list-inline social-buttons">
-          <li className="list-inline-item">
-            <div
-              className="g-ytsubscribe"
-              data-channelid="UCfk3pS8cCPxOgoleriIufyg"
-              data-layout="default"
-              data-theme="default"
-              data-count="default"></div>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://www.youtube.com/c/TorontoDeepLearningSeries" target="_blank">
-              <i className="fa fa-youtube fa-2x"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://www.reddit.com/user/tdls_to" target="_blank">
-              <i className="fa fa-reddit fa-2x"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://twitter.com/tdls_to" target="_blank">
-              <i className="fa fa-twitter fa-2x"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://github.com/TDLS" target="_blank">
-              <i className="fa fa-github fa-2x"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://www.instagram.com/tdls_to/" target="_blank">
-              <i className="fa fa-instagram fa-2x"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="mailto:tdls@torontomachinelearning.com" target="_blank">
-              <i className="fa fa-envelope fa-2x"></i>
-            </a>
-          </li>
-        </ul >
+        </div>
         <ul className="list-inline pull-right back-to-top">
           <li className="list-inline-item">
             <a href="#top">Back to top</a>
           </li>
         </ul>
-      </div >
-    </div >
+      </div>
+    </div>
   </footer>
 );
--- components/header.js
+++ components/header.js
@@ -64,6 +64,50 @@ export default () => (
             </Link>
           </li>
         </ul>
+        <ul className="list-inline social-buttons">
+          <li className="list-inline-item">
+            <div
+              className="g-ytsubscribe"
+              data-channelid="UCfk3pS8cCPxOgoleriIufyg"
+              data-layout="default"
+              data-theme="default"
+              data-count="default"
+            />
+          </li>
+          <li className="list-inline-item">
+            <a
+              href="https://www.youtube.com/c/TorontoDeepLearningSeries"
+              target="_blank"
+            >
+              <i className="fa fa-youtube fa-2x" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://www.reddit.com/user/tdls_to" target="_blank">
+              <i className="fa fa-reddit fa-2x" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://twitter.com/tdls_to" target="_blank">
+              <i className="fa fa-twitter fa-2x" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://github.com/TDLS" target="_blank">
+              <i className="fa fa-github fa-2x" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://www.instagram.com/tdls_to/" target="_blank">
+              <i className="fa fa-instagram fa-2x" />
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="mailto:tdls@torontomachinelearning.com" target="_blank">
+              <i className="fa fa-envelope fa-2x" />
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
